87eb3771a002531b7f59f79c46de5ff2cd7e4142
Gustavo Bernardo da Silva <gbsufscar@hotmail.com>
2024-01-27 07:16:45 -0300

    Classe com as informações dos episódios

--- src/main/java/br/com/alura/screenmatchspring/ScreenmatchspringApplication.java
+++ src/main/java/br/com/alura/screenmatchspring/ScreenmatchspringApplication.java
@@ -1,5 +1,6 @@
 package br.com.alura.screenmatchspring;
 
+import br.com.alura.screenmatchspring.model.DadosEpisodios;
 import br.com.alura.screenmatchspring.model.DadosSerie;
 import br.com.alura.screenmatchspring.service.ConsumoApi;
 import br.com.alura.screenmatchspring.service.ConverteDados;
@@ -21,7 +22,7 @@ public class ScreenmatchspringApplication implements CommandLineRunner {
 		ConsumoApi consumoApi = new ConsumoApi();
 
 		// Variável para receber o json da consulta
-		/// Chave do site de busca OMDb
+		/// Chave do site de busca OMDb (https://omdbapi.com/)
 		String chave = "690913ef";
 
 		/// Busca da Série
@@ -29,7 +30,7 @@ public class ScreenmatchspringApplication implements CommandLineRunner {
 
 		/// Chamar método - Consulta API para obter o json usando a biblioteca GSON
 		String endereco = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&apikey=" + chave;
-		String json = consumoApi.obterDados(endereco); // Arg - endenço com a busca
+		String json = consumoApi.obterDados(endereco); // Arg - endereço com a busca
 
 		/// Exibir o resultado da consulta
 		System.out.println("Exibir a consulta no formato json: \n" + json);
@@ -41,6 +42,37 @@ public class ScreenmatchspringApplication implements CommandLineRunner {
 		/// Exibir os dados da consulta convertido no formato desejado
 		/// Mostra representado da forma do toString do Record
 		System.out.println("Informações convertidas no formato desejado: \n" + dados);
+		System.out.println("******** \n");
+
+
+		// Busca de Informações de um Episódio
+		/// Busca da Série
+		busca ="gilmore girls"; // Já tipada anteriormente
+		String temporada = "1";
+		String episodio = "2";
+
+		/// Chamar método - Consulta API para obter o json usando a biblioteca GSON
+		endereco = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+")
+				+ "&season=" + temporada
+				+ "&episode=" + episodio
+				+ "&apikey=" + chave;
+
+		/// Obter o json da consulta
+		json = consumoApi.obterDados(endereco); // Arg - endereço com a busca
+
+		/// Exibir o resultado da consulta
+		System.out.println("Exibir a consulta no formato json: \n" + json);
+
+		/// Atualizar a variável json
+		json = consumoApi.obterDados(endereco); // Arg - endenço com a busca
+
+		/// Fazer a conversão da consulta (com conversor já instaciado acima, depois transforma o dado)
+		DadosEpisodios dadosEpisodios = conversor.obterDados(json, DadosEpisodios.class);
+
+		/// Exibir os dados da consulta convertido no formato desejado
+		/// Mostra representado da forma do toString do Record
+		System.out.println("Informações convertidas no formato desejado: \n" + dadosEpisodios);
+		System.out.println("******** \n");
 
 	}
 }
